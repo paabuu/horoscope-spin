a4c6288d16fdae666df0e6bdad41f146e98c0e2e
Wenjie Yang <wenjie.yang@ihandysoft.com>
2018-07-12 19:22:53 +0800

    gg simida

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import './index.scss';
+
+import Swipe from './swipe';
+
+// window.mySwipe = new Swipe(document.getElementById('slider'), {
+//     startSlide: 2,
+//     speed: 400,
+//     continuous: true,
+//     disableScroll: false,
+//     stopPropagation: false,
+//     callback: function(index, elem) {
+//         // console.log(index)
+//     },
+//     transitionEnd: function(index, elem) {}
+// });
+
+// function handleSwipe() {
+//     console.log('swiping');
+// }
+
+// document.body.addEventListener('touchstart', function() {
+//     document.body.addEventListener('touchmove', handleSwipe);
+// });
+
+// document.body.addEventListener('touchend', function() {
+//     document.body.removeEventListener('touchmove', handleSwipe);
+// })
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'bundle.[chunkHash:5].js',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                loader: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'postcss-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpg|svg)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192
+                    }
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].[hash:5].css'
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'index.html')
+        })
+    ]
+}
