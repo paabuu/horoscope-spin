db0d89666964de98c7d828eb3d3da65225010704
wangwf <os-wangwf@jsfund.cn>
2018-07-13 00:56:47 +0800

    almost finish spin

--- src/index.js
+++ src/index.js
@@ -25,3 +25,70 @@ import Swipe from './swipe';
 // document.body.addEventListener('touchend', function() {
 //     document.body.removeEventListener('touchmove', handleSwipe);
 // })
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+const array = ['CLINGY', 'UNDER-', 'TENDER', 'LOYAL', 'FRESH', 'AROUND'];
+
+ctx.beginPath();
+ctx.moveTo(150, 150);
+ctx.arc(150, 150, 120, 0, 2 * Math.PI);
+ctx.fillStyle = '#ffb537';
+ctx.fill();
+ctx.closePath();
+
+for (let i = 0; i < 8; i++) {
+    ctx.beginPath();
+    ctx.moveTo(150, 150);
+    ctx.arc(150, 150, 120, Math.PI / 4 * i, Math.PI / 4 * i + Math.PI / 18);
+    ctx.fillStyle = '#fff';
+    ctx.fill();
+    ctx.closePath();
+}
+
+ctx.beginPath();
+ctx.moveTo(150, 150);
+ctx.arc(150, 150, 105, 0, 2 * Math.PI);
+ctx.fillStyle = '#f1760b';
+ctx.globalAlpha = 0.8;
+ctx.fill();
+ctx.closePath();
+
+array.forEach((item, i) => {
+    ctx.beginPath();
+    ctx.globalAlpha = 1;
+    ctx.moveTo(150, 150);
+    ctx.arc(150, 150, 100, Math.PI / 3 * i, Math.PI / 3 * (i + 1));
+    ctx.fillStyle = i % 2 === 0 ? '#ff6f7a' : '#ff94ab';
+    ctx.fill();
+    ctx.closePath();
+});
+ 
+
+ctx.beginPath();
+ctx.moveTo(150, 150);
+ctx.arc(150, 150, 50, 0, 2 * Math.PI);
+ctx.fillStyle = '#000';
+ctx.globalAlpha = 0.1;
+ctx.fill();
+ctx.closePath();
+
+ctx.beginPath();
+ctx.moveTo(150, 150);
+ctx.arc(150, 150, 25, 0, 2 * Math.PI);
+ctx.globalAlpha = 1;
+ctx.fillStyle = '#fff';
+ctx.fill();
+ctx.closePath();
+
+ctx.translate(150, 150);
+array.forEach(item => {
+    ctx.fillStyle = '#fff';
+    ctx.font = '12px Airal';
+    ctx.textAlign = 'center';
+    ctx.shadowColor = 'rgba(0, 0, 0, .6)';
+    ctx.shadowOffsetX = -1;
+    ctx.shadowOffsetY = 1;
+    ctx.rotate(Math.PI / 3);
+    ctx.fillText(item, 0, -70);
+})
